18d127c3b58488fa21d20c6f36099e6c003e5acb
Camilo Contreras <crojas@pocketresult.com>
2018-11-13 20:47:14 -0500

    Ma calculette partie I

---
+++ src/index.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import "./style.css";
+import Case from './Touches'; 
+import Resultats from './Resultats'; 
+
+class Calculette extends React.Component {
+    constructor(props){
+        super(props); 
+        this.state={
+            resultat: 0, 
+            operations:[]
+        }
+        this.calculate = this.calculate.bind(this); 
+        this.addOperation = this.addOperation.bind(this); 
+    }
+    calculate(){
+        const { operations } = this.state; 
+        console.log('calculated = ' + eval(operations.join('')));
+        this.setState({
+            operations:[Math.fround(eval(operations.join('')))]
+        })
+        this.setState({
+            resultat: eval(operations.join(''))
+        })
+    }
+    
+    addOperation(op){
+        this.setState({
+            operations: this.state.operations.concat(op)
+        });
+        console.log('numéro à adtionner '+ op); 
+    }
+    renewInfo(){
+        this.setState({
+            operations:[], 
+            resultat:0
+        })
+    }
+    render() {
+        return ( <div className = "calculette" >
+            <h1 className="titleCalc"> Ma calculette </h1>
+                 <Resultats>{this.state.resultat}</Resultats>
+                <div className="laTouche">
+                {[7, 8, 9, 4, 5, 6, 1, 2, 3, 0, '.'].map((c) => <Case key={c} onClick={() =>this.addOperation(c)}> {c}</Case>)}
+                <Case className="result" onClick={this.calculate}>= </Case>
+                </div>
+                <div className="operateurs">
+                {['/', '*', '-', '+'].map((c)=><Case key={c} onClick={()=> this.addOperation(c)}>{c}</Case>)}
+                <div className="renew" onClick={()=> this.renewInfo()}>CE</div></div>
+                
+                
+            </div>
+        );
+    }
+}
+ReactDom.render( <Calculette /> ,
+    document.getElementById('app')
+);
